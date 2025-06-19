53d106d472678bfe1a0c86cc16edfe6eb518be8b
albermatica <informaticotuamigo@gmail.com>
2025-06-19 18:28:26 +0200

    Lección 2.1: Refactorización de nombres y uso de interpolación de strings.

--- HolaDotNet/Program.cs
+++ HolaDotNet/Program.cs
@@ -6,14 +6,17 @@ miPrimerProducto.Nombre = "Laptop Gamer";
 miPrimerProducto.Precio = 1200.00;
 miPrimerProducto.Stock = 5;
 
-Producto MiSegundoProducto = new Producto();
-MiSegundoProducto.Id = 2;
-MiSegundoProducto.Nombre = "Monitor Ultrawide";
-MiSegundoProducto.Precio = 3600.00;
-MiSegundoProducto.Stock = 3;
+Producto miSegundoProducto = new Producto();
+miSegundoProducto.Id = 2;
+miSegundoProducto.Nombre = "Monitor Ultrawide";
+miSegundoProducto.Precio = 3600.00;
+miSegundoProducto.Stock = 3;
 
 Console.WriteLine("Producto: " + miPrimerProducto.Nombre);
 Console.WriteLine("Precio: " + miPrimerProducto.Precio);
+
+Console.WriteLine("Producto 2: " + miSegundoProducto.Nombre);
+Console.WriteLine("Precio 2: " + miSegundoProducto.Precio);
 
 Console.WriteLine("\nPresiona cualquier tecla para salir...");
 Console.ReadKey();
